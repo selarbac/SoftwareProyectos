da2a3e91529785042efd7ccc9526ca220b385127
ErikCrisP96 <crisanto_e96@hotmail.com>
2018-02-16 23:07:36 -0600

    Añadiendo Look and Feel a la ventana

--- Nutrika/src/Vistas/ReporteAntropo.java
+++ Nutrika/src/Vistas/ReporteAntropo.java
@@ -6,16 +6,27 @@
 package Vistas;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 /**
  *
  * @author crisa
  */
 public class ReporteAntropo {
+
     public static void main(String[] args) {
+        try {
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Repo_Antropo vista = new Repo_Antropo();
         JFrame frame = new JFrame("Evaluacion Antropometrica");
-        frame.setSize(1180, 500);
+        frame.setSize(1180, 600);
         frame.setLocation(100, 100);
         frame.add(vista);
         frame.setVisible(true);
